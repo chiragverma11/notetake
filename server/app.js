498ccde83d0def6fe1c3a0db620e47935c9f1700
Chirag Verma <chiragverma1013@gmail.com>
2023-06-08 13:15:41 +0530

    refactor: changes cors in app.js

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ import * as dotenv from "dotenv";
 const app = express();
 
 //dotenv configuration
+
 if (process.env.NODE_ENV !== "PRODUCTION") {
   dotenv.config({ path: "config/.env" });
 }
@@ -15,18 +16,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 //Allowed Origins separated with string to Array
 const ALLOWED_ORIGINS = process.env.ALLOWED_ORIGINS?.split(",");
 //Cors
-process.env.NODE_ENV !== "PRODUCTION"
-  ? app.use(
-      cors({
-        credentials: true,
-        origin: [
-          "http://localhost:5173",
-          "http://127.0.0.1:5173",
-          "http://192.168.1.6:5173",
-        ],
-      })
-    )
-  : app.use(
+app.use(
       cors({
         credentials: true,
         origin: ALLOWED_ORIGINS,
@@ -42,6 +32,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 //Routes Imports
 import user from "./routes/userRoute.js";
 import note from "./routes/noteRoute.js";
@@ -49,13 +40,6 @@ import note from "./routes/noteRoute.js";
 app.use("/api", user);
 app.use("/api", note);
 
-//Router from express router
-const router = express.Router();
-
-//Just to show message when direct api is visited
-router.get("/", (req, res) => {
-  res.send("Server is Running");
-});
 
 //Error Middleware For Error Handling
 app.use(errorMiddleware);
